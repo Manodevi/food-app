2bd38fbd563ad45a8420e92c5d9173424becc6b2
sathishkumar <y2jsathish@hotmail.com>
2021-08-16 18:03:43 +0400

    Created context to manage cart items

---
+++ src/store/cart-context.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Creating context to manage cart items
+const initialContext = [
+  {
+    items: [],
+    totalAmount: 0,
+    addItem: (item) => {},
+    removeItem: (id) => {},
+    updateItem: (item) => {}
+  }
+];
+
+const CartContext = React.createContext(initialContext);
+
+export default CartContext;
