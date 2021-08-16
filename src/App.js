73ae073f2f541673a20871ef9c9baa1d61794315
sathishkumar <smanodevi@gmail.com>
2021-08-16 18:24:59 +0400

    Wrapping Cart context Provider around the App component - to make cart items available for all other components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { Fragment } from 'react';
 import Header from './components/Layout/Header';
 import AppInfo from './components/Meals/AppInfo';
 import MealsList from './components/Meals/MealsList';
+import CartProvider from './store/CartProvider';
 
 function App() {
   return (
-    <Fragment>
+    <CartProvider>
       <Header />
       <main className = "main-content">
         <AppInfo />
         <MealsList />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
