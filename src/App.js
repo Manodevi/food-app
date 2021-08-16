5d87a32ab8ee7fdca4348f9f4086cc98d67f5454
Manodevi <88968771+Manodevi@users.noreply.github.com>
2021-08-16 11:25:59 +0400

    Added new Components App Info and MealsList

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Fragment } from 'react';
 import Header from './components/Layout/Header';
+import AppInfo from './components/Meals/AppInfo';
+import MealsList from './components/Meals/MealsList';
 
 function App() {
   return (
-    <Header />
+    <Fragment>
+      <Header />
+      <main className = "main-content">
+        <AppInfo />
+        <MealsList />
+      </main>
+    </Fragment>
   );
 }
 
