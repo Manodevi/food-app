a4884732142e3f51705d277b0d84054bc63c2365
Manodevi <88968771+Manodevi@users.noreply.github.com>
2021-08-16 11:35:50 +0400

    Added components for app info and meals list

---
+++ src/components/Meals/AppInfo.js
@@ -0,0 +1,20 @@
+import Card from '../UI/Card';
+
+const AppInfo = () => {
+  return (
+    <section id = "app-info">
+      <Card className = "card-dark app-info">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris imperdiet enim sit amet lorem dictum pellentesque.
+          Nunc hendrerit dolor sit.        
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis lectus a urna maximus 
+          venenatis. Quisque accumsan tempor eros vitae lobortis. Suspendisse sit amet congue diam. Pellentesque habitant morbi tristique.
+        </p>
+      </Card>
+    </section>
+  );
+};
+
+export default AppInfo;
