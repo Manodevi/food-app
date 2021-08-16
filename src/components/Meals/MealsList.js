492aec87e2f02764c8fcad15e363844e4e774c3c
sathishkumar <y2jsathish@hotmail.com>
2021-08-16 11:21:07 +0400

    Created new components for Meals List and app info

---
+++ src/components/Meals/MealsList.js
@@ -0,0 +1,16 @@
+import Card from '../UI/Card';
+
+const MealsList = () => {
+  return (
+    <Card className = "card-light meals-list">
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
+    </Card>
+  );
+};
+
+export default MealsList;
