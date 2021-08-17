744052d05bd772c939e329f379fa86518174743e
sathishkumar <smanodevi@gmail.com>
2021-08-17 13:06:00 +0400

    Added useReducer to handle cart State with items and total Amount Also added logic to reducer function just for the add item.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,20 +1,44 @@
 import CartContext from './cart-context';
+import React, { useReducer } from 'react';
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+  if(action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedAmount = state.totalAmount + action.item.price * action.item.count;
+    
+    return {
+      items: updatedItems,
+      totalAmount: updatedAmount
+    }
+  }
+  return defaultCartState;
+};
 
 // Seperate component for context provider to manage all actions in one file
 const CartProvider = props => {
-  const addItemHandler = item => {console.log(item)};
+  const [cartState, cartDispatch] = useReducer(cartReducer, defaultCartState);
+
+  const addItemHandler = (id, item) => {
+    cartDispatch({ type: 'ADD', id, item });
+  };
 
   const removeItemHandler = id => {};
 
   const updateItemHandler = item => {};
 
   const cartContextValue = {
-      items: [],
-      totalAmount: 0,
+      items: cartState.items,
+      totalAmount: cartState.totalAmount,
       addItem: addItemHandler,
       removeItem: removeItemHandler,
       updateItem: updateItemHandler
     };
+
   return (
     <CartContext.Provider value = {cartContextValue}>
       {props.children}
