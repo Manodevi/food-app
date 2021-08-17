e02fb19740533dfd760e6ac77bc99409b7e50048
sathishkumar <smanodevi@gmail.com>
2021-08-17 20:11:23 +0400

    Retrieved cart items details from cart context and passing it to the cart item

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,23 +1,39 @@
 import Card from '../UI/Card';
 import Button from '../UI/Button';
+import CartContext from '../../store/cart-context';
+import { useContext } from 'react';
+import CartItem from './CartItem';
 
 const Cart = props => {
+  // to get details from cart context
+  const cartCtx = useContext(CartContext);
+  const items = [...cartCtx.items];
+  const totalAmount = cartCtx.totalAmount;
+
+  let haveItems = false;
+  if(items.length > 0) {
+    haveItems = true;
+  }
 
   return (    
-      <Card className = "cart-modal card-light">
-        <p>No items in the cart</p>
-        <Button button = {
-            { 
-              className: 'btn btn-white',
-              onClick: props.onClose
+      <Card className = "cart-modal card-light">        
+        <CartItem items = {items} totalAmount = {totalAmount} />
+        
+        { !haveItems && (<p>No items in the cart</p>) }
+        <div className = "cart-buttons">
+          <Button button = {
+              { 
+                className: 'btn btn-white',
+                onClick: props.onClose
+              }
             }
-          }
-          >Close</Button>
-        <Button button = {
-            {
-              className: 'btn btn-primary'
-            }
-          }>Order</Button>
+            >Close</Button>
+            {haveItems && <Button button = {
+              {
+                className: 'btn btn-primary'
+              }
+            }>Order</Button>}
+          </div>
       </Card>
   );
 };
