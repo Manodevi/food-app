deb1555bbce90e3e2399a088f78cfb2283a1f140
sathishkumar <smanodevi@gmail.com>
2021-08-17 17:04:48 +0400

    Passing pointer to the Cart button - onclick to show the cart modal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,12 @@
 import CartButton from "./CartButton"
 
-const Header = () => {
+const Header = props => {
   return (
     <header className = "header">
       <nav id = "nav-head">
         <div className = "header-content">
           <h1>React Food App</h1>
-          <CartButton />
+          <CartButton onClick = {props.onShowCart} />
         </div>
       </nav>
     </header>
