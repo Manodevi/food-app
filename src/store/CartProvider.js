9e66d7f3a6b4614edc54cd2e5daf8dd55d98f7a0
sathishkumar <smanodevi@gmail.com>
2021-08-18 11:18:49 +0400

    Added the complete add item functionality to the cart reducer function in Cart Context - now can able to add from item list and also update it from cart modal

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,10 +7,30 @@ const defaultCartState = {
 }
 
 const cartReducer = (state, action) => {
-  if(action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
-    const updatedAmount = state.totalAmount + action.item.price * action.item.count;
-    
+  if (action.type === 'ADD') {
+    const existingItemIndex = state.items.findIndex(item => item.id === action.item.id);
+    let existingItem = state.items[existingItemIndex];
+
+    let updatedItems;
+    let updatedAmount;
+
+    if (existingItem) {
+      updatedItems = [...state.items];
+      const updatedItem = {...existingItem};
+      if(action.from === 'CART') {       
+        updatedItem.count = updatedItem.count + 1;
+        updatedAmount = state.totalAmount + action.item.price * 1;
+      } else {
+        updatedItem.count = updatedItem.count + action.item.count;
+        updatedAmount = state.totalAmount + action.item.price * action.item.count;
+      }
+      
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+      updatedAmount = state.totalAmount + action.item.price * action.item.count;
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedAmount
@@ -23,8 +43,8 @@ const cartReducer = (state, action) => {
 const CartProvider = props => {
   const [cartState, cartDispatch] = useReducer(cartReducer, defaultCartState);
 
-  const addItemHandler = (id, item) => {
-    cartDispatch({ type: 'ADD', id, item });
+  const addItemHandler = (item, from) => {
+    cartDispatch({ type: 'ADD', item, from});
   };
 
   const removeItemHandler = id => {};
