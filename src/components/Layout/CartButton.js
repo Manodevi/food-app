acd94f90ca902f3d604a3b296594978e806cc020
sathishkumar <smanodevi@gmail.com>
2021-08-18 16:22:27 +0400

    useEffect - to make header cart button bumping animation 0.3s when updating the cart using the state and cart items dependency and clearing out the state on clean up, updating the cart header with updated total number of items

--- src/components/Layout/CartButton.js
+++ src/components/Layout/CartButton.js
@@ -1,19 +1,49 @@
 import Button from '../UI/Button';
 import CartContext from '../../store/cart-context';
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 
 const CartButton = props => {
   const CartCtx = React.useContext(CartContext);
 
+  const { items } = CartCtx;
+  const [cartBtnHighlighted, setCartBtnHighlighted] = useState(false);
+
+  // updating the cart header with updated total number of items
+  const totalItems = CartCtx.items.reduce((itemCount, item) => {
+      return itemCount + item.count;
+  }, 0);
+
+  // useEffect - to make header cart button bumping animation 0.3s when updating the cart
+  // using the state and cart items dependency and clearing out the state on clean up
+  useEffect(() => {
+    if (items.length > 0) {
+      setCartBtnHighlighted(true);
+    }
+    const btnTimer = setTimeout(() => {
+      setCartBtnHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(btnTimer);
+    }    
+  }, [items]);  // items dependency
+
+  // adding animation class based on the btn highlight state
+  const btnClasses = `cart-header btn ${cartBtnHighlighted ? 'btn-animation' : ''}`;
+
   const buttonProps = {
     button: {
-      className: "cart-header btn", type: "button", onClick: props.onClick
+      title: "My Cart",
+      className: btnClasses, type: "button", onClick: props.onClick
     }
   };
   return (
     <Button {...buttonProps}>
-      <span>Cart</span>
-      <span id = "cart-items">{CartCtx.totalAmount}</span>
+      <i className = "fas fa-shopping-cart"></i>
+      <span>My Cart</span>
+      {items.length > 0 && (
+          <span id = "cart-items">{ totalItems }</span>
+      )}
     </Button>
   );
 };
