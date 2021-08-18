5117d5bb6602eafa525b18059ebb678c4bc402fe
sathishkumar <smanodevi@gmail.com>
2021-08-18 11:55:40 +0400

    Added title to the buttons by default value as its children

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,5 +1,7 @@
 const Button = props => {
-  return <button {...props.button}>{props.children}</button>
+  return <button 
+            title = {props.children}
+            {...props.button}>{props.children}</button>
 };
 
 export default Button;
