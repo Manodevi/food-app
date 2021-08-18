1f838dd98aef143ded2e8c94c4c2c77162ff1e27
sathishkumar <smanodevi@gmail.com>
2021-08-18 11:54:46 +0400

    Added remove item functionality in cart reducer function - when cart context is called from cart modal

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,6 +7,8 @@ const defaultCartState = {
 }
 
 const cartReducer = (state, action) => {
+
+  // Add items to the cart state
   if (action.type === 'ADD') {
     const existingItemIndex = state.items.findIndex(item => item.id === action.item.id);
     let existingItem = state.items[existingItemIndex];
@@ -14,19 +16,19 @@ const cartReducer = (state, action) => {
     let updatedItems;
     let updatedAmount;
 
-    if (existingItem) {
+    if (existingItem) {   // If items already exist in state, update that particular item
       updatedItems = [...state.items];
       const updatedItem = {...existingItem};
-      if(action.from === 'CART') {       
+      if(action.from === 'CART') {       // update count by 1 if it is from Cart Modal
         updatedItem.count = updatedItem.count + 1;
         updatedAmount = state.totalAmount + action.item.price * 1;
-      } else {
+      } else {    // It is from the meals list item
         updatedItem.count = updatedItem.count + action.item.count;
         updatedAmount = state.totalAmount + action.item.price * action.item.count;
       }
       
       updatedItems[existingItemIndex] = updatedItem;
-    } else {
+    } else {    // Not already exist, so add the item newly to the cart state
       updatedItems = state.items.concat(action.item);
       updatedAmount = state.totalAmount + action.item.price * action.item.count;
     }
@@ -36,6 +38,36 @@ const cartReducer = (state, action) => {
       totalAmount: updatedAmount
     }
   }
+
+  // Remove item from the cart state
+  if(action.type === 'REMOVE') {
+    const existingItemIndex = state.items.findIndex(item => {
+          return item.id === action.id; 
+        }
+      );
+
+    const existingItem = state.items[existingItemIndex];
+    const updatedAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+
+    if(existingItem.count === 1) { // If the removing item has only one count
+      updatedItems = state.items.filter(item => item.id !== existingItem.id);      
+    } else {  // else if has more count - just remove 1 count from it
+      const updatedItem = {...existingItem};
+      updatedItem.count -= 1;      
+      updatedItems = [...state.items];
+      updatedItems[existingItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedAmount
+    }
+
+  }
+
+
+
   return defaultCartState;
 };
 
@@ -44,10 +76,12 @@ const CartProvider = props => {
   const [cartState, cartDispatch] = useReducer(cartReducer, defaultCartState);
 
   const addItemHandler = (item, from) => {
-    cartDispatch({ type: 'ADD', item, from});
+    cartDispatch({ type: 'ADD', item, from });
   };
 
-  const removeItemHandler = id => {};
+  const removeItemHandler = id => {
+    cartDispatch({ type: 'REMOVE', id });
+  };
 
   const updateItemHandler = item => {};
 
