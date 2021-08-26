15fadb3a878c03c70a3552d439ff3b53ee654bad
sathishkumar <smanodevi@gmail.com>
2021-08-26 16:41:18 +0400

    Retrieve dummy list from different file

--- src/components/Meals/MealsList.js
+++ src/components/Meals/MealsList.js
@@ -1,43 +1,7 @@
 import Card from '../UI/Card';
 import MealsListItem from './MealsListItem';
+import DUMMY_LIST from './MealsDummyList';
 
-const DUMMY_LIST = [
-  {
-    id: 'meals_1',
-    name: 'Sushi',
-    description: 'Lorem ipsum dolor sit amet',
-    price: 23,
-    img: '/assets/img/Cheese_Lover_Beef.jpg'
-  },
-  {
-    id: 'meals_2',
-    name: 'Shawarma',
-    description: 'Lorem ipsum sdsd sit amet',
-    price: 45.50,
-    img: '/assets/img/Cheese_Lover_Beef.jpg'
-  },
-  {
-    id: 'meals_4',
-    name: 'Briyani',
-    description: 'Lorem dolor sit amet',
-    price: 25.90,
-    img: '/assets/img/Cheese_Lover_Beef.jpg'
-  },
-  {
-    id: 'meals_5',
-    name: 'Mac Soup',
-    description: 'Lorem ipsum dolor sit amet',
-    price: 12.99,
-    img: '/assets/img/Cheese_Lover_Beef.jpg'
-  },
-  {
-    id: 'meals_6',
-    name: 'Veg Roll',
-    description: 'Lorem ipsum dolor sit amet',
-    price: 10,
-    img: '/assets/img/Cheese_Lover_Beef.jpg'
-  },
-];
 const MealsList = () => {
   const list = DUMMY_LIST.map(item => 
             <MealsListItem {...item} key = {item.id} />
