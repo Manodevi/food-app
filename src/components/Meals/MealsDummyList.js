dcc2d08e8839ffc9e19aeb4a4655079a33499674
sathishkumar <smanodevi@gmail.com>
2021-08-26 16:51:33 +0400

    Added some real meal list for better UI

--- src/components/Meals/MealsDummyList.js
+++ src/components/Meals/MealsDummyList.js
@@ -2,38 +2,31 @@
 const DUMMY_LIST = [
   {
     id: 'meals_1',
-    name: 'Sushi',
-    description: 'Lorem ipsum dolor sit amet',
-    price: 23,
-    img: '/assets/img/Cheese_Lover_Beef.jpg'
+    name: 'Kebbe Nayeh',
+    description: 'Fresh raw meat with crushed wheat(served with fresh onions, mint, garlic sauce, fresh bread.',
+    price: 50,
+    img: '/assets/img/Kebbe_nayae.webp'
   },
   {
     id: 'meals_2',
-    name: 'Shawarma',
-    description: 'Lorem ipsum sdsd sit amet',
-    price: 45.50,
-    img: '/assets/img/Cheese_Lover_Beef.jpg'
-  },
-  {
-    id: 'meals_4',
-    name: 'Briyani',
-    description: 'Lorem dolor sit amet',
-    price: 25.90,
-    img: '/assets/img/Cheese_Lover_Beef.jpg'
+    name: 'Kaaket Turkey and Cheese',
+    description: "Traditional Lebanese kaak filled with cheese and turkey",
+    price: 23,
+    img: '/assets/img/kaaket_turkey_and_cheese.webp'
   },
   {
     id: 'meals_5',
-    name: 'Mac Soup',
-    description: 'Lorem ipsum dolor sit amet',
-    price: 12.99,
-    img: '/assets/img/Cheese_Lover_Beef.jpg'
+    name: 'Kaaket Nutella Banana',
+    description: 'Toasted Lebanese kaak filled with nutella and banana',
+    price: 21,
+    img: '/assets/img/kaaket_nutella_banana.webp'
   },
   {
     id: 'meals_6',
-    name: 'Veg Roll',
-    description: 'Lorem ipsum dolor sit amet',
-    price: 10,
-    img: '/assets/img/Cheese_Lover_Beef.jpg'
+    name: 'Kaaket Picon',
+    description: 'Toasted Lebanese kaak filled with traditional picon cheese sprinkled with sumac',
+    price: 15,
+    img: '/assets/img/kaaket_picon.jpeg'
   },
 ];
 
