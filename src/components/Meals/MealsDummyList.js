d937d83f221dfcec95f99601c09f60d6930942f5
sathishkumar <smanodevi@gmail.com>
2021-08-26 16:40:31 +0400

    Dummy meals list variable to different file to make the file look leaner

---
+++ src/components/Meals/MealsDummyList.js
@@ -0,0 +1,40 @@
+
+const DUMMY_LIST = [
+  {
+    id: 'meals_1',
+    name: 'Sushi',
+    description: 'Lorem ipsum dolor sit amet',
+    price: 23,
+    img: '/assets/img/Cheese_Lover_Beef.jpg'
+  },
+  {
+    id: 'meals_2',
+    name: 'Shawarma',
+    description: 'Lorem ipsum sdsd sit amet',
+    price: 45.50,
+    img: '/assets/img/Cheese_Lover_Beef.jpg'
+  },
+  {
+    id: 'meals_4',
+    name: 'Briyani',
+    description: 'Lorem dolor sit amet',
+    price: 25.90,
+    img: '/assets/img/Cheese_Lover_Beef.jpg'
+  },
+  {
+    id: 'meals_5',
+    name: 'Mac Soup',
+    description: 'Lorem ipsum dolor sit amet',
+    price: 12.99,
+    img: '/assets/img/Cheese_Lover_Beef.jpg'
+  },
+  {
+    id: 'meals_6',
+    name: 'Veg Roll',
+    description: 'Lorem ipsum dolor sit amet',
+    price: 10,
+    img: '/assets/img/Cheese_Lover_Beef.jpg'
+  },
+];
+
+export default DUMMY_LIST;
