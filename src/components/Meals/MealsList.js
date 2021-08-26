902cf42f003f46538dd2fd035f98c62c6be72a86
sathishkumar <smanodevi@gmail.com>
2021-08-26 17:00:14 +0400

    Added state for meals list as dummy meals as default and retrieve dynamic meals from google firebase backend using Fetch API

--- src/components/Meals/MealsList.js
+++ src/components/Meals/MealsList.js
@@ -1,9 +1,39 @@
+import { useState, useEffect } from 'react';
 import Card from '../UI/Card';
 import MealsListItem from './MealsListItem';
 import DUMMY_LIST from './MealsDummyList';
 
+const FETCH_URL = 'https://react-food-app-e5bb3-default-rtdb.firebaseio.com/meals.json';
+
 const MealsList = () => {
-  const list = DUMMY_LIST.map(item => 
+  const [loadedMeals, setLoadedMeals] = useState(DUMMY_LIST);
+  useEffect(() => {    
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch(FETCH_URL);
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }       
+        const responseData = await response.json();        
+        const mealsList = [];
+        
+        for (let key in responseData) {
+          mealsList.push({
+            id: key,
+            name: responseData[key].name,
+            description: responseData[key].description,
+            price: responseData[key].price,
+            img: `/assets/img/${responseData[key].img}`
+          });
+        }
+        setLoadedMeals(mealsList);
+      } catch (error) {        
+      }
+    };
+    fetchMeals();
+  }, []);
+
+  const list = loadedMeals.map(item => 
             <MealsListItem {...item} key = {item.id} />
             );
   return (
